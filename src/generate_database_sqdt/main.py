f73270ed0f2f4839e5991d62c7379aec611a0979
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-10-21 17:40:10 +0200

    allow for hyperfine

--- src/generate_database_sqdt/main.py
+++ src/generate_database_sqdt/main.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import argparse
 import logging
 import os
@@ -9,12 +11,13 @@ from typing import TYPE_CHECKING
 
 import pandas as pd
 import ryd_numerov
-from ryd_numerov import RydbergStateAlkali
+from ryd_numerov import RydbergStateAlkaliHyperfine
 
 from generate_database_sqdt import __version__, database_sql_file
 from generate_database_sqdt.generate_misc import create_tables_for_misc
 from generate_database_sqdt.utils import (
     calc_matrix_element_one_pair,
+    element_from_species,
     get_rydberg_state_cached,
     get_sorted_list_of_states,
 )
@@ -38,7 +41,7 @@ class WarningsAsExceptionsHandler(logging.Handler):
 logger = logging.getLogger(__name__)
 
 
-MATRIX_ELEMENTS_OF_INTEREST: dict[str, "MatrixElementType"] = {  # key: (operator, k_radial, k_angular)
+MATRIX_ELEMENTS_OF_INTEREST: dict[str, MatrixElementType] = {  # key: (operator, k_radial, k_angular)
     "matrix_elements_d": "ELECTRIC_DIPOLE",
     "matrix_elements_q": "ELECTRIC_QUADRUPOLE",
     "matrix_elements_o": "ELECTRIC_OCTUPOLE",
@@ -160,13 +163,14 @@ def create_tables_for_one_species(
         populate_matrix_elements_table(list_of_states, conn, max_delta_n, all_n_up_to)
     logger.info("Size of %s: %.6f megabytes", db_file, db_file.stat().st_size * 1e-6)
 
+    element = element_from_species(species)
     with sqlite3.connect(db_file) as conn:
         for tkey in ["states", *MATRIX_ELEMENTS_OF_INTEREST.keys()]:
             parquet_file = Path(f"{tkey}.parquet")
             table = pd.read_sql_query(f"SELECT * FROM {tkey}", conn)
             if tkey == "states":
                 table = table.astype({"is_j_total_momentum": bool, "is_calculated_with_mqdt": bool})
-                table["is_j_total_momentum"] = True
+                table["is_j_total_momentum"] = element.i_c == 0
                 table["is_calculated_with_mqdt"] = False
             table.to_parquet(parquet_file, index=False, compression="zstd")
             logger.info("Size of %s: %.6f megabytes", parquet_file, parquet_file.stat().st_size * 1e-6)
@@ -178,14 +182,12 @@ def create_tables_for_one_species(
     logger.info("get_rydberg_state_cached: %s", get_rydberg_state_cached.cache_info())
 
 
-def populate_states_table(list_of_states: list[RydbergStateAlkali], conn: "sqlite3.Connection") -> None:
+def populate_states_table(list_of_states: list[RydbergStateAlkaliHyperfine], conn: sqlite3.Connection) -> None:
     """Populate the states table with data for a given species."""
     states_data = []
     for ids, state in enumerate(list_of_states):
-        exp_j_ryd = state.angular.to_state().calc_exp_qn("j_r")
-        std_j_ryd = state.angular.to_state().calc_std_qn("j_r")
-
-        n_star = state.get_nu()
+        angular_ket = state.angular
+        angular_state = state.angular.to_state()
 
         states_data.append(
             (
@@ -193,20 +195,20 @@ def populate_states_table(list_of_states: list[RydbergStateAlkali], conn: "sqlit
                 state.element.get_ionization_energy() + state.get_energy("a.u."),  # energy
                 (-1) ** state.l,  # parity = (-1)^l
                 state.n,  # n: quantum number
-                n_star,  # nu = NStar for sqdt
-                state.j,  # f: quantum number, neglect hyperfine splitting -> f = j
-                n_star,  # exp_nui = nu for sqdt
-                state.l,  # exp_l = l
-                state.j,  # exp_j = j
-                state.angular.s_tot,  # exp_s = s
-                state.l,  # exp_l_ryd = l for sqdt
-                exp_j_ryd,  # exp_j_ryd = j for sqdt only one valence electron
+                state.get_nu(),  # nu = NStar for sqdt
+                angular_ket.f_tot,  # f: quantum number
+                state.get_nu(),  # exp_nui = nu for sqdt
+                angular_ket.l_tot,  # exp_l = l
+                angular_ket.j_tot,  # exp_j = j
+                angular_ket.s_tot,  # exp_s = s
+                angular_ket.l_r,  # exp_l_ryd = l for sqdt
+                angular_state.calc_exp_qn("j_r"),  # exp_j_ryd = j for sqdt only one valence electron
                 0,  # std_nui = 0
                 0,  # std_l = 0
                 0,  # std_j = 0
                 0,  # std_s = 0
                 0,  # std_l_ryd = 0
-                std_j_ryd,  # std_j_ryd = 0 for sqdt and only one valence electron
+                angular_state.calc_std_qn("j_r"),  # std_j_ryd = 0 for sqdt and only one valence electron
                 "True",  # is_j_total_momentum = True for no hyperfine splitting
                 "False",  # is_calculated_with_mqdt = False for sqdt
                 0,  # underspecified_channel_contribution = 0 for sqdt
@@ -218,21 +220,19 @@ def populate_states_table(list_of_states: list[RydbergStateAlkali], conn: "sqlit
 
 
 def populate_matrix_elements_table(
-    list_of_states: list[RydbergStateAlkali], conn: "sqlite3.Connection", max_delta_n: int, all_n_up_to: int
+    list_of_states: list[RydbergStateAlkaliHyperfine], conn: sqlite3.Connection, max_delta_n: int, all_n_up_to: int
 ) -> None:
     k_angular_max = 3
 
-    element = list_of_states[0].element
-    list_of_qns = [(ids, state.n, state.l, state.j) for ids, state in enumerate(list_of_states)]
-
+    list_of_id_state = [(ids, state) for ids, state in enumerate(list_of_states)]
     # sort the states by l, n for more efficient caching
-    qns_sorted_by_l = sorted(list_of_qns, key=lambda x: (x[2], x[1], x[0]))
+    list_of_id_state = sorted(list_of_id_state, key=lambda x: (x[1].l, x[1].n, x[0]))
 
     matrix_elements: dict[str, list[tuple[int, int, float]]] = {tkey: [] for tkey in MATRIX_ELEMENTS_OF_INTEREST}
-    for i, (id1, n1, l1, j1) in enumerate(qns_sorted_by_l):
-        qns_filtered = filter(lambda x: x[2] - l1 <= k_angular_max, qns_sorted_by_l[i:])
-        for id2, n2, l2, j2 in qns_filtered:
-            if all(n > all_n_up_to for n in [n1, n2]) and abs(n1 - n2) > max_delta_n:
+    for i, (id1, state1) in enumerate(list_of_id_state):
+        list_filtered = filter(lambda x: x[1].l - state1.l <= k_angular_max, list_of_id_state[i:])
+        for id2, state2 in list_filtered:
+            if all(n > all_n_up_to for n in [state1.n, state2.n]) and abs(state1.n - state2.n) > max_delta_n:
                 # If delta_n is larger than max_delta_n, we dont calculate the matrix elements anymore,
                 # since these are so small, that they are usually not relevant for further calculations
                 # However, we keep all dipole interactions with small n (we choose all_n_up_to as a cutoff)
@@ -240,15 +240,15 @@ def populate_matrix_elements_table(
                 continue
 
             id_tuple = (id1, id2) if id1 <= id2 else (id2, id1)
-            qns = (n1, l1, j1, n2, l2, j2) if id1 <= id2 else (n2, l2, j2, n1, l1, j1)
-            me_one_pair = calc_matrix_element_one_pair(element.species, *qns, MATRIX_ELEMENTS_OF_INTEREST)
+            states = (state1, state2) if id1 <= id2 else (state2, state1)
+
+            me_one_pair = calc_matrix_element_one_pair(states[0], states[1], MATRIX_ELEMENTS_OF_INTEREST)
             for tkey, me in me_one_pair.items():
                 matrix_elements[tkey].append((*id_tuple, me))
 
             if id1 != id2:
                 id_tuple = (id_tuple[1], id_tuple[0])
-                qns = qns[3:] + qns[:3]
-                me_one_pair = calc_matrix_element_one_pair(element.species, *qns, MATRIX_ELEMENTS_OF_INTEREST)
+                me_one_pair = calc_matrix_element_one_pair(states[1], states[0], MATRIX_ELEMENTS_OF_INTEREST)
                 for tkey, me in me_one_pair.items():
                     matrix_elements[tkey].append((*id_tuple, me))
 
