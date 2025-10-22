47f32b482369d837363b68b2903487b8df11a830
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-10-22 11:23:44 +0200

    fix pairinteraction database.sql

--- src/generate_database_sqdt/__init__.py
+++ src/generate_database_sqdt/__init__.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 import pairinteraction
 
-database_sql_file = Path(pairinteraction.__file__).parent / "_wrapped" / "database" / "database.sql"
+database_sql_file = Path(pairinteraction.__file__).parent / "database" / "database.sql"
 
 __version__ = "1.3"
