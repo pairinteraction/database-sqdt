58fcef1846983cd064f18288bc0e861a54fb822c
johannes-moegerle <johannes.moegerle@itp3.uni-stuttgart.de>
2025-11-10 16:19:14 +0100

    fix slow generate misc

--- src/generate_database_sqdt/generate_misc.py
+++ src/generate_database_sqdt/generate_misc.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 import rydstate
-from rydstate.angular.utils import calc_wigner_3j
+from rydstate.angular.utils import calc_wigner_3j, calc_wigner_3j_with_symmetries
 
 from generate_database_sqdt import __version__, database_sql_file
 
@@ -40,19 +40,20 @@ def create_tables_for_misc(f_max: float, kappa_max: int) -> None:
 def populate_wigner_table(f_max: float, kappa_max: int, conn: "sqlite3.Connection") -> None:
     """Populate the wigner table with data for all wigner symbols up to f_max and kappa_max."""
     wigner_data = []
-    for kappa in range(kappa_max + 1):
-        for q in range(-kappa, kappa + 1):
-            for f_initial in np.arange(0, f_max + 0.5, 0.5):
+    for start_f_max in [0, 0.5]:  # for better caching
+        for kappa in range(kappa_max + 1):
+            for f_initial in np.arange(start_f_max, f_max + 0.5, 1):
                 for f_final in np.arange(np.max([f_initial % 1, f_initial - kappa]), f_initial + kappa + 1):
-                    for m_initial in np.arange(-f_initial, f_initial + 1):
-                        m_final = m_initial + q
-                        if not -f_final <= m_final <= f_final:
-                            continue
-                        wigner = calc_wigner_3j(f_final, kappa, f_initial, -m_final, q, m_initial)
-                        wigner *= (-1) ** (f_final - m_final)
-                        if wigner == 0:
-                            continue
-                        wigner_data.append((f_initial, f_final, m_initial, m_final, kappa, q, wigner))
+                    for q in range(-kappa, kappa + 1):
+                        for m_initial in np.arange(-f_initial, f_initial + 1):
+                            m_final = m_initial + q
+                            if not -f_final <= m_final <= f_final:
+                                continue
+                            wigner = calc_wigner_3j_with_symmetries(f_final, kappa, f_initial, -m_final, q, m_initial)  # type: ignore [arg-type]
+                            wigner *= (-1) ** (f_final - m_final)
+                            if wigner == 0:
+                                continue
+                            wigner_data.append((f_initial, f_final, m_initial, m_final, kappa, q, wigner))
 
     conn.executemany(f"INSERT INTO wigner VALUES ({', '.join(['?'] * len(wigner_data[0]))})", wigner_data)
     logger.info("Created the 'wigner' table (%s rows)", conn.execute("SELECT COUNT(*) FROM wigner").fetchone()[0])
